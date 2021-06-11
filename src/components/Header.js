b7427c5e3affdc852dbb0968c59404289936274b
Rishi <rishi18454@gmail.com>
2021-06-11 18:29:08 +0530

    edit, delete, create btns added. create btn routed

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ const Header = function () {
 
       <div className="collapse navbar-collapse" id="navbarsExample04">
         <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
+          <li className="nav-item">
             <Link to="/" className="nav-link">
               Streams
             </Link>
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { showStreamList } from "../../actions";
 import "../../css/streamList.css";
 
@@ -8,12 +9,40 @@ class StreamList extends React.Component {
     this.props.showStreamList();
   }
 
+  renderButtons = (authorId) => {
+    const currentUserId = this.props.currentUserId;
+    if (currentUserId !== authorId) {
+      return null;
+    }
+    return (
+      <React.Fragment>
+        <button type="button" className="btn btn-outline-primary mr-2 mr-md-3">
+          Edit
+        </button>
+        <button type="button" className="btn btn-outline-danger float-right">
+          Delete
+        </button>
+      </React.Fragment>
+    );
+  };
+
+  renderCreateBtn = () => {
+    if (!this.props.isSignedIn) {
+      return null;
+    }
+    return (
+      <Link to="/streams/create" className="btn btn-primary btn-lg btn-block">
+        Create Stream
+      </Link>
+    );
+  };
+
   renderedList = (streams) => {
     return streams.map((stream) => {
       return (
         <li className="list-group-item " key={stream.id}>
-          <div className="row">
-            <div className="col-2 col-md-1 ">
+          <div className="row justify-content-center">
+            <div className="col-2 col-md-1  ">
               <i
                 className="fas fa-camera fa-2x icon-container"
                 style={{ width: "100%" }}
@@ -23,6 +52,7 @@ class StreamList extends React.Component {
               <h5 className="mb-1">{stream.title}</h5>
               <p className="mb-1">{stream.description}</p>
             </div>
+            <div>{this.renderButtons(stream.userId)}</div>
           </div>
         </li>
       );
@@ -30,9 +60,12 @@ class StreamList extends React.Component {
   };
   render() {
     return (
-      <div className="container mt-3">
+      <div className="container mt-3 ">
         <h3 className="text-center">Available Streams</h3>
-        <ul className="list-group">{this.renderedList(this.props.streams)}</ul>
+        <ul className="list-group stream-list-container mb-4">
+          {this.renderedList(this.props.streams)}
+        </ul>
+        {this.renderCreateBtn()}
       </div>
     );
   }
@@ -41,6 +74,8 @@ class StreamList extends React.Component {
 const mapStateToProps = (state) => {
   return {
     streams: Object.values(state.streams),
+    currentUserId: state.auth.id,
+    isSignedIn: state.auth.isSignedIn,
   };
 };
 
