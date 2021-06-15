2b88c77f1cb6f87fd48867134e26a9ad2d64ab98
rishi dubey <rishi18454@gmail.com>
2021-06-15 14:58:04 +0530

    live stream implemented

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,11 +1,40 @@
 import React from "react";
 import { connect } from "react-redux";
 import { showStream } from "../../actions";
+import flv from "flv.js";
 
 class StreamShow extends React.Component {
-  componentDidMount() {
-    this.props.showStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
   }
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.showStream(id);
+    this.createVidPlayer();
+  }
+
+  componentDidUpdate() {
+    this.createVidPlayer();
+  }
+
+  componentWillUnmount() {
+    this.vidPlayer.destroy();
+  }
+
+  createVidPlayer() {
+    if (!this.props.stream || this.vidPlayer) {
+      return;
+    }
+    const { id } = this.props.match.params;
+    this.vidPlayer = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.vidPlayer.attachMediaElement(this.videoRef.current);
+    this.vidPlayer.load();
+  }
+
   render() {
     if (!this.props.stream) {
       return <div className="container">...Loading</div>;
@@ -13,6 +42,13 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream;
     return (
       <div className="container">
+        <video
+          id="videoElement"
+          ref={this.videoRef}
+          style={{ width: "100%" }}
+          controls
+          className="mt-3"
+        />
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
